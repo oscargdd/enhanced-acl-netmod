24d4616906544803f4f051e9800dd36a87c8f5a9
Med <mohamed.boucadair@orange.com>
2023-03-08 16:38:32 +0100

    Update yang.mk

--- yang/yang.mk
+++ yang/yang.mk
@@ -6,9 +6,9 @@ $(STDYANGDIR):
 
 OPTIONS=--tree-print-groupings --tree-no-expand-uses -f tree --tree-line-length=70
 ifeq ($(OS), Windows_NT)
-YANG_PATH="$(YANGDIR);$(STDYANGDIR)/standard/ietf/RFC/;$(STDYANGDIR)/standard/ieee/published/802.1/;$(STDYANGDIR)/experimental/ietf-extracted-YANG-modules"
+YANG_PATH="$(YANGDIR);$(STDYANGDIR)/standard/ietf/RFC/;$(STDYANGDIR)/experimental/ietf-extracted-YANG-modules"
 else
-YANG_PATH="$(YANGDIR):$(STDYANGDIR)/standard/ietf/RFC/:$(STDYANGDIR)/standard/ieee/published/802.1/:$(STDYANGDIR)/experimental/ietf-extracted-YANG-modules"
+YANG_PATH="$(YANGDIR):$(STDYANGDIR)/standard/ietf/RFC/:$(STDYANGDIR)/experimental/ietf-extracted-YANG-modules"
 endif
 YANG=$(wildcard $(YANGDIR)/*.yang)
 STDYANG=$(wildcard $(YANGDIR)/ietf-*.yang)
@@ -22,7 +22,7 @@ pyang-lint: $(STDYANG) $(EXPYANG) $(STDYANGDIR)
 	pyang -V -p $(YANG_PATH) $(EXPYANG)
 
 yang-lint: $(STDYANG) $(STDYANGDIR)
-	yanglint --verbose -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/standard/ieee/published/802.1/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules $(STDYANG) -i
+	yanglint --verbose -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules $(STDYANG) -i
 
 yang-gen-diagram: yang-lint $(TXT)
 
